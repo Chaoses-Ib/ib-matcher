09aaff8a97b9ca1312edc0fdf596a84eb700d9a8
Chaoses-Ib <Chaos-es@outlook.com>
2025-08-12 02:57:47 +0800

    fix(matcher/regex): `backtrack` and `nfa` doctest

--- ib-matcher/src/regex/mod.rs
+++ ib-matcher/src/regex/mod.rs
@@ -5,7 +5,6 @@
 //!
 //! The backtrack engine is forked from [`regex_automata::nfa::thompson::backtrack`](https://docs.rs/regex-automata/0.4.9/regex_automata/nfa/thompson/backtrack/index.html).
 
-#[cfg(not(doctest))]
 pub mod backtrack;
 mod nfa;
 #[cfg(feature = "regex-syntax")]
